49743324362118c4628306e7d3a132052979a831
Vinicius Vaz <vvaz@macbook-pro-7.redventures.net>
2018-05-21 14:33:21 -0300

    - QueryParser

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,12 @@ const port = 9009
     , express = require('express')
     , server = express()
     , allowCors = require('./cors')
+    , queryParser = require('express-query-int')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser())
 
 server.listen(port, () => {
     console.log(`Backend Port: ${port}.`)
